fc0a1ddde9dacfcdb742c657ec223a2d9f7377a2
ma10s <mattis.janitz@nav.no>
2019-09-24 17:20:44 +0200

    CORS allowed origins

--- src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
@@ -8,8 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        val corsRegistration = registry.addMapping("/api/**")
-        corsRegistration.allowedOrigins("https://familie-ef-soknad.nais.oera-q.local")
+        val corsRegistration = registry.addMapping("/**")
+        corsRegistration
+                .allowedOrigins("https://familie-ef-soknad.nais.oera-q.local")
+                .allowCredentials(true)
     }
 
 
