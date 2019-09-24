51f95b534c4e58b395cab8d309fda94beec6fcce
ma10s <mattis.janitz@nav.no>
2019-09-24 17:04:35 +0200

    Vil legge til CORS allowed origins

---
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
@@ -0,0 +1,16 @@
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        val corsRegistration = registry.addMapping("/api/**")
+        corsRegistration.allowedOrigins("https://familie-ef-soknad.nais.oera-q.local")
+    }
+
+
+}
