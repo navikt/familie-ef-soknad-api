471b50ae5e2d7efafbc8746a72145db085f06180
ma10s <mattis.janitz@nav.no>
2019-09-25 10:22:46 +0200

    CORS allowed origins

--- src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
@@ -1,18 +1,32 @@
+package no.nav.familie.ef.søknad.config
+
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+import org.springframework.context.annotation.Profile
+
+
 @Configuration
-@EnableWebMvc
-class WebConfig : WebMvcConfigurer {
+@Profile("dev")
+class WebConfigDev :  WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST")
+                        .allowCredentials(true).maxAge(3600)
+            }
+        }
 
+@Configuration
+@Profile("!dev")
+class WebConfig :  WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        val corsRegistration = registry.addMapping("/**")
-        corsRegistration
+        registry.addMapping("/api/**")
                 .allowedOrigins("https://familie-ef-soknad.nais.oera-q.local")
-                .allowCredentials(true)
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true).maxAge(3600)
     }
-
-
 }
+
+
