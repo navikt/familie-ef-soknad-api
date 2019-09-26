d920ad06d04badca15953bfbbf9833466cdf510f
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-09-26 10:30:23 +0200

    Legg til enkel get for testing

--- src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
@@ -4,10 +4,7 @@ import no.nav.familie.ef.søknad.api.dto.Kvittering
 import no.nav.familie.ef.søknad.api.dto.Søknad
 import no.nav.familie.ef.søknad.service.SøknadService
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping(path = ["/api/soknad"], produces = [APPLICATION_JSON_VALUE])
@@ -15,6 +12,12 @@ class SøknadController(val søknadService: SøknadService) {
 
     @PostMapping("sendInn")
     fun sendInn(@RequestBody søknad: Søknad): Kvittering {
+        return søknadService.sendInn(søknad)
+    }
+
+    @GetMapping("test")
+    fun testMottak(): Kvittering {
+        val søknad = Søknad("")
         return søknadService.sendInn(søknad)
     }
 
