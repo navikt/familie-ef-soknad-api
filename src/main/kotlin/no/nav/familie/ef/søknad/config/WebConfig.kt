94b3c1402206804b7242928e7d15b38e3079a5d7
ma10s <mattis.janitz@nav.no>
2019-09-26 12:24:14 +0200

    Legg til CORS mapping -> /**, ikke /api/**

--- src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/WebConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfigDev(private val corsProperties: CorsProperties) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(*corsProperties.allowedOrigins)
                 .allowedMethods("GET", "POST")
                 .allowCredentials(true).maxAge(3600)
