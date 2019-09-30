38dc27e55802b44dd3d8103e14d8cec9443bd239
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-09-30 12:15:21 +0200

    test endepunkt

--- src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
@@ -3,6 +3,7 @@ package no.nav.familie.ef.søknad.api
 import no.nav.familie.ef.søknad.api.dto.Kvittering
 import no.nav.familie.ef.søknad.api.dto.Søknad
 import no.nav.familie.ef.søknad.service.SøknadService
+import no.nav.security.oidc.api.Unprotected
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 
@@ -15,6 +16,7 @@ class SøknadController(val søknadService: SøknadService) {
         return søknadService.sendInn(søknad)
     }
 
+    @Unprotected
     @GetMapping("test")
     fun testMottak(): Kvittering {
         val søknad = Søknad("")
