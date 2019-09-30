a6e865867ac492dec315e791b43a13d6a26a048d
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-09-30 12:24:56 +0200

    Fjern endepunkt brukt for å teste kontakt

--- src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/SøknadController.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ef.søknad.api
 import no.nav.familie.ef.søknad.api.dto.Kvittering
 import no.nav.familie.ef.søknad.api.dto.Søknad
 import no.nav.familie.ef.søknad.service.SøknadService
-import no.nav.security.oidc.api.Unprotected
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.bind.annotation.*
 
@@ -15,12 +14,4 @@ class SøknadController(val søknadService: SøknadService) {
     fun sendInn(@RequestBody søknad: Søknad): Kvittering {
         return søknadService.sendInn(søknad)
     }
-
-    @Unprotected
-    @GetMapping("test")
-    fun testMottak(): Kvittering {
-        val søknad = Søknad("")
-        return søknadService.sendInn(søknad)
-    }
-
 }
