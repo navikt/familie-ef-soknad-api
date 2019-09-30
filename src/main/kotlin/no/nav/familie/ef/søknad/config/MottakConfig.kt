4c54343eb68f2f081decc4cb9d562cd2e1a3d69e
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-09-30 12:01:03 +0200

    Fiks bad variable name error (kan ikke ha punktum)

--- src/main/kotlin/no/nav/familie/ef/søknad/config/MottakConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/MottakConfig.kt
@@ -9,8 +9,8 @@ import java.net.URI
 //@ConfigurationProperties(prefix = "søknad", ignoreUnknownFields = false)
 @Component
 internal data class MottakConfig(@Value("\${familie.ef.mottak.url}") val url: String,
-                                 @Value("\${familie.ef.mottak.username}") val username: String,
-                                 @Value("\${familie.ef.mottak.password}") val password: String) {
+                                 @Value("\${FAMILIE_EF_MOTTAK_USERNAME}") val username: String,
+                                 @Value("\${FAMILIE_EF_MOTTAK_PASSWORD}") val password: String) {
 
     val sendInnUri: URI get() = DefaultUriBuilderFactory().uriString(url).path(PATH_SEND_INN).build()
 
