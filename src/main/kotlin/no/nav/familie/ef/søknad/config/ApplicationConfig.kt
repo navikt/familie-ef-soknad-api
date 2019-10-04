5757ddc38e4ad9dc7c910d56aee17303803581a6
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-10-04 13:56:14 +0200

    test id'er med commons logfilter

--- src/main/kotlin/no/nav/familie/ef/søknad/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/ApplicationConfig.kt
@@ -1,8 +1,11 @@
 package no.nav.familie.ef.søknad.config
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import no.nav.familie.log.filter.LogFilter
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestOperations
@@ -10,10 +13,22 @@ import org.springframework.web.client.RestOperations
 @SpringBootConfiguration
 class ApplicationConfig {
 
+    private val LOG = LoggerFactory.getLogger(ApplicationConfig::class.java)
+
     @Bean
     fun restTemplate(vararg interceptors: ClientHttpRequestInterceptor): RestOperations =
             RestTemplateBuilder().interceptors(*interceptors).build()
 
     @Bean
     fun kotlinModule(): KotlinModule = KotlinModule()
+
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        LOG.info("Registering LogFilter filter")
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.setFilter(LogFilter())
+        filterRegistration.order = 1
+        return filterRegistration
+    }
 }
--- src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
@@ -9,21 +9,28 @@ import org.springframework.web.client.RestOperations
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import no.nav.familie.log.IdUtils.generateId
 import no.nav.familie.http.client.NavHttpHeaders
+import no.nav.familie.log.mdc.MDCConstants
+import org.slf4j.MDC
 
 
 @Service
 internal class SøknadClient(operations: RestOperations,
                    private val config: MottakConfig) : PingableRestClient(operations, config.pingUri) {
 
+    val NAV_CALLID_HEADER = NavHttpHeaders.NAV_CALLID.asString()
+    val NAV_CONSUMER_HEADER = NavHttpHeaders.NAV_CONSUMER_ID.asString()
+    val NAV_USERID_HEADER = NavHttpHeaders.NAV_PERSONIDENT.asString()
+
     fun sendInn(søknadDto: SøknadDto): KvitteringDto {
         log.info("Sender inn søknad")
-        val callId = generateId()
+        log.info(MDCConstants.MDC_CALL_ID + " konstant har verdi " + MDC.get(MDCConstants.MDC_CALL_ID) + " og settter til header " + NAV_CALLID_HEADER)
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.set(config.username, config.password)
-        headers.set(NavHttpHeaders.NAV_CALLID.asString(), callId)
+        headers.set(NAV_CONSUMER_HEADER, MDC.get(MDCConstants.MDC_CONSUMER_ID))
+        headers.set(NAV_CALLID_HEADER, MDC.get(MDCConstants.MDC_CALL_ID))
+        headers.set(NAV_USERID_HEADER, MDC.get(MDCConstants.MDC_USER_ID))
         val body = ObjectMapper().writeValueAsString(søknadDto)
         val entity = HttpEntity(body, headers)
 
