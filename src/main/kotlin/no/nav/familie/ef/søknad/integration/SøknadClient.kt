97a1e571857e769535674cbb5a53963563fb8066
Nina Kjekstad <nina.kjekstad@bekk-mac-01818.local>
2019-10-04 15:22:48 +0200

    logging

--- src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
@@ -10,7 +10,7 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import no.nav.familie.http.client.NavHttpHeaders
-import no.nav.familie.log.mdc.MDCConstants
+import no.nav.familie.log.mdc.MDCConstants.*
 import org.slf4j.MDC
 
 
@@ -19,18 +19,17 @@ internal class SøknadClient(operations: RestOperations,
                    private val config: MottakConfig) : PingableRestClient(operations, config.pingUri) {
 
     val NAV_CALLID_HEADER = NavHttpHeaders.NAV_CALLID.asString()
-    val NAV_CONSUMER_HEADER = NavHttpHeaders.NAV_CONSUMER_ID.asString()
+    val NAV_CONSUMERID_HEADER = NavHttpHeaders.NAV_CONSUMER_ID.asString()
     val NAV_USERID_HEADER = NavHttpHeaders.NAV_PERSONIDENT.asString()
-
     fun sendInn(søknadDto: SøknadDto): KvitteringDto {
         log.info("Sender inn søknad")
-        log.info(MDCConstants.MDC_CALL_ID + " konstant har verdi " + MDC.get(MDCConstants.MDC_CALL_ID) + " og settter til header " + NAV_CALLID_HEADER)
+        log.info(MDC_REQUEST_ID + " "+ MDC.get(MDC_REQUEST_ID) + ", " + MDC_CONSUMER_ID + " "+ MDC.get(MDC_CONSUMER_ID) + ", " + MDC_CALL_ID + " "+ MDC.get(MDC_CALL_ID) + ", " + MDC_USER_ID + " " + MDC.get(MDC_USER_ID) )
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.set(config.username, config.password)
-        headers.set(NAV_CONSUMER_HEADER, MDC.get(MDCConstants.MDC_CONSUMER_ID))
-        headers.set(NAV_CALLID_HEADER, MDC.get(MDCConstants.MDC_CALL_ID))
-        headers.set(NAV_USERID_HEADER, MDC.get(MDCConstants.MDC_USER_ID))
+        headers.set(NAV_CONSUMERID_HEADER, MDC.get(MDC_CONSUMER_ID))
+        headers.set(NAV_CALLID_HEADER, MDC.get(MDC_CALL_ID))
+        headers.set(NAV_USERID_HEADER, MDC.get(MDC_USER_ID))
         val body = ObjectMapper().writeValueAsString(søknadDto)
         val entity = HttpEntity(body, headers)
 
