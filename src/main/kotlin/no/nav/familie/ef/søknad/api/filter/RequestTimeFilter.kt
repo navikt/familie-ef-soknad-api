b0f205bf309b6c1c9d779913e6af5bc67c5010a3
Nina Kjekstad <ninakjeks@gmail.com>
2019-10-09 10:46:35 +0200

    fiks casting exception

--- src/main/kotlin/no/nav/familie/ef/søknad/api/filter/RequestTimeFilter.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/filter/RequestTimeFilter.kt
@@ -1,28 +1,29 @@
 package no.nav.familie.ef.søknad.api.filter
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.client.ClientHttpResponse
 import org.springframework.util.StopWatch
+import java.io.IOException
 
 import javax.servlet.*
-import java.io.IOException
+import javax.servlet.http.*
+
+
 
 class RequestTimeFilter : Filter {
 
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse, filterChain: FilterChain) {
 
-        val response = servletResponse as ClientHttpResponse
-        val request = servletRequest as HttpRequest
+        val response = servletResponse as HttpServletResponse
+        val request = servletRequest as HttpServletRequest
         val timer = StopWatch()
         try {
             timer.start()
         }
         finally {
             timer.stop()
-            log(request, response.statusCode, timer)
+            log(request, response.status, timer)
         }
         filterChain.doFilter(servletRequest, servletResponse)
     }
@@ -31,16 +32,17 @@ class RequestTimeFilter : Filter {
     companion object {
         private val LOG = LoggerFactory.getLogger(RequestTimeFilter::class.java)
 
-        private fun log(request: HttpRequest, code: HttpStatus, timer: StopWatch) {
+        private fun log(request: HttpServletRequest, code: Int, timer: StopWatch) {
             if (hasError(code)) {
-                LOG.warn("{} - {} - ({}). Dette tok {}ms", request.methodValue, request.uri.path, code, timer.totalTimeMillis)
+                LOG.warn("{} - {} - ({}). Dette tok {}ms", request.method, request.requestURI, code, timer.totalTimeMillis)
             } else {
-                LOG.info("{} - {} - ({}). Dette tok {}ms", request.methodValue, request.uri.path, code, timer.totalTimeMillis)
+                LOG.info("{} - {} - ({}). Dette tok {}ms", request.method, request.requestURI, code, timer.totalTimeMillis)
             }
         }
 
-        private fun hasError(code: HttpStatus): Boolean {
-            return code.series() == HttpStatus.Series.CLIENT_ERROR || code.series() == HttpStatus.Series.SERVER_ERROR
+        private fun hasError(code: Int): Boolean {
+            val series = HttpStatus.Series.resolve(code)
+            return series == HttpStatus.Series.CLIENT_ERROR || series == HttpStatus.Series.SERVER_ERROR
         }
     }
 }
