2d30fb9d5ea2717a239c570b2153bbb22bb86cf5
Nina Kjekstad <ninakjek@users.noreply.github.com>
2019-10-10 12:15:00 +0200

    Legg til id'er på requester
    * benytter `LogFilter` fra nav/familie common for å legge id'er på requester
    * timer requester med filter i stedet for interceptors (se https://github.com/navikt/familie-ef-soknad-api/pull/9)
    * krever credentials til mottak gw i dev og prod

--- src/main/kotlin/no/nav/familie/ef/søknad/config/ApplicationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/ApplicationConfig.kt
@@ -1,19 +1,43 @@
 package no.nav.familie.ef.søknad.config
 
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import no.nav.familie.ef.søknad.api.filter.RequestTimeFilter
+import no.nav.familie.log.filter.LogFilter
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
-import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestOperations
 
 @SpringBootConfiguration
 class ApplicationConfig {
 
+    private val LOG = LoggerFactory.getLogger(ApplicationConfig::class.java)
+
     @Bean
-    fun restTemplate(vararg interceptors: ClientHttpRequestInterceptor): RestOperations =
-            RestTemplateBuilder().interceptors(*interceptors).build()
+    fun restTemplate(): RestOperations =
+            RestTemplateBuilder().build()
 
     @Bean
     fun kotlinModule(): KotlinModule = KotlinModule()
+
+
+    @Bean
+    fun logFilter(): FilterRegistrationBean<LogFilter> {
+        LOG.info("Registering LogFilter filter")
+        val filterRegistration = FilterRegistrationBean<LogFilter>()
+        filterRegistration.filter = LogFilter()
+        filterRegistration.order = 1
+        return filterRegistration
+    }
+
+    @Bean
+    fun requestTimeFilter(): FilterRegistrationBean<RequestTimeFilter> {
+        LOG.info("Registering RequestTimeFilter filter")
+        val filterRegistration = FilterRegistrationBean<RequestTimeFilter>()
+        filterRegistration.filter = RequestTimeFilter()
+        filterRegistration.order = 2
+        return filterRegistration
+    }
 }
--- src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClient.kt
@@ -9,19 +9,22 @@ import org.springframework.web.client.RestOperations
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-
-
+import no.nav.familie.http.client.NavHttpHeaders
+import no.nav.familie.log.mdc.MDCConstants.*
+import org.slf4j.MDC
 
 
 @Service
 internal class SøknadClient(operations: RestOperations,
-                   private val config: MottakConfig) : PingableRestClient(operations, config.pingUri) {
+                            private val config: MottakConfig) : PingableRestClient(operations, config.pingUri) {
+
+    val NAV_CALLID_HEADER = NavHttpHeaders.NAV_CALLID.asString()
 
     fun sendInn(søknadDto: SøknadDto): KvitteringDto {
-
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.set(config.username, config.password)
+        headers.set(NAV_CALLID_HEADER, MDC.get(MDC_CALL_ID))
         val body = ObjectMapper().writeValueAsString(søknadDto)
         val entity = HttpEntity(body, headers)
 
