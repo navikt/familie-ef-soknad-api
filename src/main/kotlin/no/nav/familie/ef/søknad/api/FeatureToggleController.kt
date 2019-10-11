aad5a80889caa43f07068caeae95f6b8bf15aabb
Nina Kjekstad <ninakjeks@gmail.com>
2019-10-11 13:52:15 +0200

    endepunkt for alle funksjonsbrytere til frontend

--- src/main/kotlin/no/nav/familie/ef/søknad/api/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/FeatureToggleController.kt
@@ -8,6 +8,14 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping(path = ["/api/featuretoggle"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class FeatureToggleController(private val featureToggleService: FeatureToggleService) {
 
+    val funksjonsbrytere = listOf("familie.ef.soknad.send-soknad")
+
+    @GetMapping
+    fun sjekkAlle() : Map<String, Boolean> {
+        val toggleVerdier = funksjonsbrytere.map { toggle -> toggle to featureToggleService.isEnabled(toggle)}.toMap()
+        return toggleVerdier
+    }
+
     @GetMapping("/{toggleId}")
     fun sjekkFunksjonsbryter(@PathVariable toggleId: String,
                              @RequestParam("defaultverdi") defaultVerdi: Boolean? = false): Boolean {
