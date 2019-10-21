ad615aab217bb48c0f3da48dd845f3feb877dca8
Bent Lorentzen <bent.lorentzen@nav.no>
2019-10-21 15:20:09 +0200

    Lagt til Metrics.counter på helsesjekkene

--- src/main/kotlin/no/nav/familie/ef/søknad/health/AbstractHealthIndicator.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/health/AbstractHealthIndicator.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.søknad.health
 
+import io.micrometer.core.instrument.Counter
 import no.nav.familie.ef.søknad.config.HealthIndicatorConfig
 import no.nav.familie.ef.søknad.integration.PingableRestClient
 import org.springframework.boot.actuate.health.Health
@@ -8,12 +9,17 @@ import org.springframework.boot.actuate.health.HealthIndicator
 internal abstract class AbstractHealthIndicator(private val pingable: PingableRestClient,
                                                 private val healthIndicatorConfig: HealthIndicatorConfig) : HealthIndicator {
 
+    protected abstract val successCounter: Counter
+    protected abstract val failureCounter: Counter
+
 
     override fun health(): Health {
         return try {
             pingable.ping()
+            successCounter.increment()
             up()
         } catch (e: Exception) {
+            failureCounter.increment()
             down(e)
         }
     }
--- src/main/kotlin/no/nav/familie/ef/søknad/health/OppslagHealthIndicator.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/health/OppslagHealthIndicator.kt
@@ -1,9 +1,15 @@
 package no.nav.familie.ef.søknad.health
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Metrics
 import no.nav.familie.ef.søknad.config.HealthIndicatorConfig
 import no.nav.familie.ef.søknad.integration.OppslagClient
 import org.springframework.stereotype.Component
 
 @Component
 internal class OppslagHealthIndicator(oppslagClient: OppslagClient, healthIndicatorConfig: HealthIndicatorConfig)
-    : AbstractHealthIndicator(oppslagClient, healthIndicatorConfig)
+    : AbstractHealthIndicator(oppslagClient, healthIndicatorConfig) {
+
+    override val successCounter: Counter = Metrics.counter("tps.innsyn.health", "response", "success")
+    override val failureCounter: Counter = Metrics.counter("tps.innsyn.health", "response", "failure")
+}
