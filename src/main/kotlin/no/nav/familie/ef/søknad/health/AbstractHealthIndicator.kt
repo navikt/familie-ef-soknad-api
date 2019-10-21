6da8f513f9132a6555301215b20c9b61558b2c71
Bent Lorentzen <bent.lorentzen@nav.no>
2019-10-21 15:45:47 +0200

    Lagt til logging i helsesjekker.

--- src/main/kotlin/no/nav/familie/ef/søknad/health/AbstractHealthIndicator.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/health/AbstractHealthIndicator.kt
@@ -3,23 +3,28 @@ package no.nav.familie.ef.søknad.health
 import io.micrometer.core.instrument.Counter
 import no.nav.familie.ef.søknad.config.HealthIndicatorConfig
 import no.nav.familie.ef.søknad.integration.PingableRestClient
+import org.slf4j.LoggerFactory
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 
 internal abstract class AbstractHealthIndicator(private val pingable: PingableRestClient,
                                                 private val healthIndicatorConfig: HealthIndicatorConfig) : HealthIndicator {
 
+    private val logger= LoggerFactory.getLogger(this::class.java)
     protected abstract val successCounter: Counter
     protected abstract val failureCounter: Counter
 
 
+
     override fun health(): Health {
+        logger.info("Pinging ${pingable.pingURI()}")
         return try {
             pingable.ping()
             successCounter.increment()
             up()
         } catch (e: Exception) {
             failureCounter.increment()
+            logger.warn("Kan ikke kontakte ${pingable.pingURI()}")
             down(e)
         }
     }
