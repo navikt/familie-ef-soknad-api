a401d73d5352472a2c3e5075a0580e4a3343e436
ma10s <mattis.janitz@nav.no>
2019-10-23 12:02:19 +0200

    Add Authentication option to swagger2.

--- src/main/kotlin/no/nav/familie/ef/søknad/config/SwaggerDocumentationConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/config/SwaggerDocumentationConfig.kt
@@ -6,7 +6,11 @@ import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 
 
@@ -26,11 +30,33 @@ class SwaggerDocumentationConfig {
                 .apis(RequestHandlerSelectors.basePackage(basePackage))
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContext())
                 .apiInfo(apiInfo())
+    }
+
+    private fun securitySchemes(): List<ApiKey> {
+        return listOf(ApiKey("Bearer", "Authorization", "header"))
+    }
+
+    private fun securityContext(): List<SecurityContext> {
+        return listOf(SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/api.*"))
+                .build())
+    }
+
+    private fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return listOf(SecurityReference("Bearer", authorizationScopes))
     }
 
     private fun apiInfo(): ApiInfo {
         return ApiInfoBuilder().build()
     }
+
+
 }
 
