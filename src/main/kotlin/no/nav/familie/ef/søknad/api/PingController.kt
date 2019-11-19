ce698ccea398c13ebb6d41a1895c0d197a0f0ac5
Nina Kjekstad <ninakjeks@gmail.com>
2019-11-19 13:34:35 +0100

    Legg til endepunkt for å sjekke om man er autentisert. Fjernet også protection på ping-endepunkt som tidligere ble brukt til dette

--- src/main/kotlin/no/nav/familie/ef/søknad/api/PingController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/PingController.kt
@@ -1,32 +1,18 @@
 package no.nav.familie.ef.søknad.api
 
-import no.nav.familie.ef.søknad.api.dto.Søknad
-import no.nav.familie.ef.søknad.util.InnloggingUtils
-import no.nav.security.token.support.core.api.ProtectedWithClaims
-
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@Unprotected
 @RequestMapping(path = ["/api"], produces = [MediaType.APPLICATION_JSON_VALUE])
-@ProtectedWithClaims(issuer = InnloggingUtils.ISSUER, claimMap = ["acr=Level4"])
 class PingController {
 
     @GetMapping("/ping")
     fun ping(): PingDto {
         return PingDto("Ack - vi har kontakt")
     }
-
-    @PostMapping("/ping", consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun pingPost(@RequestBody request: Søknad): PingDto {
-        return PingDto(" Ack - vi har mottatt: ${request.text} ${InnloggingUtils.fødselsnummer}")
-    }
-
-    @GetMapping("/getToken")
-    fun getToken(): PingDto {
-        return PingDto(InnloggingUtils.fødselsnummer)
-    }
-
 }
 
 data class PingDto(val message: String)
