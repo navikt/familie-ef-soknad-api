d973a4c2efc1a86b31002f4a61310f0d8d46df29
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-13 16:21:10 +0100

    fjern duplikate variabler og debug

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -2,10 +2,10 @@ package no.nav.familie.ef.søknad
 
 import no.nav.familie.ef.søknad.util.getFileAsString
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.util.Properties
+import java.util.*
 
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ef.søknad"])
 @EnableSwagger2
@@ -13,12 +13,12 @@ import java.util.Properties
 class Application
 
 fun main(args: Array<String>) {
-
     val props = Properties()
-    /*
     props.put("familie.ef.mottak.passord", getFileAsString("/secrets/apikey/familie-integrasjoner"))
     props.put("familie.integrasjoner.passord", getFileAsString("/secrets/apikey/familie-ef-mottak"))
     props.put("tps.innsyn.passord", getFileAsString("/secrets/apikey/tps-innsyn"))
-     */
-    SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
+
+    val app = SpringApplication(Application::class.java)
+    app.setDefaultProperties(props)
+    app.run(*args)
 }
