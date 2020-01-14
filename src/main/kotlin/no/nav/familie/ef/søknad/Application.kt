5cc5942a3ec86606d3beb2bbeea4844e485ba8c9
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-14 10:16:47 +0100

    build før run

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -4,6 +4,7 @@ import no.nav.familie.ef.søknad.util.getFileAsString
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.*
 
@@ -13,12 +14,24 @@ import java.util.*
 class Application
 
 fun main(args: Array<String>) {
+
+    /*
     val props = Properties()
+
     props.put("familie.ef.mottak.passord", getFileAsString("/secrets/apikey/familie-integrasjoner"))
     props.put("familie.integrasjoner.passord", getFileAsString("/secrets/apikey/familie-ef-mottak"))
     props.put("tps.innsyn.passord", getFileAsString("/secrets/apikey/tps-innsyn"))
+    //SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
 
-    val app = SpringApplication(Application::class.java)
-    app.setDefaultProperties(props)
-    app.run(*args)
+
+    //val app = SpringApplication.run(Application::class.java, *args)
+    //app.environment.setDefaultProfiles(props)
+    //app.run(Application::class.java, *args)
+    //SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
+
+     */
+
+    SpringApplicationBuilder(Application::class.java).build().run(*args)
+    // SpringApplication.run(Application::class.java, *args)
+
 }
