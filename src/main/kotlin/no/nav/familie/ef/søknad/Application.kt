e6e448a02a4eb6f3305bd5086999384f463f937c
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-14 12:50:24 +0100

    opp et nivå for å hente kv

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -16,14 +16,14 @@ class Application
 fun main(args: Array<String>) {
 
 
-    /*
+
     val props = Properties()
 
-    props.put("familie.ef.mottak.passord", getFileAsString("/secrets/apikey/familie-integrasjoner"))
-    props.put("familie.integrasjoner.passord", getFileAsString("/secrets/apikey/familie-ef-mottak"))
-    props.put("tps.innsyn.passord", getFileAsString("/secrets/apikey/tps-innsyn"))
-    SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
-    */
+    props.put("familie.ef.mottak.passord", getFileAsString("/secrets/apikey/familie-integrasjoner/x-nav-apiKey"))
+    props.put("familie.integrasjoner.passord", getFileAsString("/secrets/apikey/familie-ef-mottak/x-nav-apiKey"))
+    props.put("tps.innsyn.passord", getFileAsString("/secrets/apikey/tps-innsyn/x-nav-apiKey"))
+    //SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
+
 
     //val app = SpringApplication(Application::class.java)
     //app.setDefaultProperties(props)
