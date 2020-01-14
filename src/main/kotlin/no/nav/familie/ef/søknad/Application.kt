54f73bd0e366ebc6cf8ff095055944e41d54be15
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-14 15:40:18 +0100

    kjør med secrets

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -2,7 +2,6 @@ package no.nav.familie.ef.søknad
 
 import no.nav.familie.ef.søknad.util.getFileAsString
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -14,20 +13,9 @@ import java.util.*
 class Application
 
 fun main(args: Array<String>) {
-
-
-
     val props = Properties()
-
     props.put("familie.ef.mottak.passord", getFileAsString("/secrets/apikey/familie-integrasjoner/x-nav-apiKey"))
     props.put("familie.integrasjoner.passord", getFileAsString("/secrets/apikey/familie-ef-mottak/x-nav-apiKey"))
     props.put("tps.innsyn.passord", getFileAsString("/secrets/apikey/tps-innsyn/x-nav-apiKey"))
-    //SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
-
-
-    //val app = SpringApplication(Application::class.java)
-    //app.setDefaultProperties(props)
-    //app.run(*args)
-    SpringApplication.run(Application::class.java, *args)
-
+    SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
 }
