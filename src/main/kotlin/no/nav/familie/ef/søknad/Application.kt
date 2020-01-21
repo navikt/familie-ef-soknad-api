1528a74c97890168652bdde063f6c30071335fcb
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-21 16:43:49 +0100

    kubernetes secrets

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -2,6 +2,7 @@ package no.nav.familie.ef.søknad
 
 import no.nav.familie.ef.søknad.util.getFileAsString
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -13,9 +14,10 @@ import java.util.*
 class Application
 
 fun main(args: Array<String>) {
-    val props = Properties()
-    props["familie.ef.mottak.passord"] = getFileAsString("/secrets/apikey/familie-ef-mottak/x-nav-apiKey")
-    props["familie.integrasjoner.passord"] = getFileAsString("/secrets/apikey/familie-integrasjoner/x-nav-apiKey")
-    props["tps.innsyn.passord"] = getFileAsString("/secrets/apikey/tps-innsyn/x-nav-apiKey")
-    SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
+    //val props = Properties()
+    //props["familie.ef.mottak.passord"] = getFileAsString("/secrets/apikey/familie-ef-mottak/x-nav-apiKey")
+    //props["familie.integrasjoner.passord"] = getFileAsString("/secrets/apikey/familie-integrasjoner/x-nav-apiKey")
+    //props["tps.innsyn.passord"] = getFileAsString("/secrets/apikey/tps-innsyn/x-nav-apiKey")
+    //SpringApplicationBuilder(Application::class.java).properties(props).run(*args)
+    SpringApplication.run(Application::class.java, *args)
 }
