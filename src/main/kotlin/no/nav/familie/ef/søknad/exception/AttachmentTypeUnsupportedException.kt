ac60b5bfa3e69707a03585d3a48a3a6bc4d07c46
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-21 10:24:01 +0100

    Fjern kode relatert til mellomlagring/vedlegg. Kan hekte på dokument-app i egen oppgave

--- src/main/kotlin/no/nav/familie/ef/søknad/exception/AttachmentTypeUnsupportedException.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.familie.ef.søknad.exception
-
-import org.springframework.http.MediaType
-
-class AttachmentTypeUnsupportedException(mediaType: MediaType?,
-                                         msg: String? = "Media type $mediaType er ikke støttet",
-                                         e: Throwable? = null)
-    : AttachmentException(mediaType, e, msg)
--- src/main/kotlin/no/nav/familie/ef/søknad/exception/AttachmentVirusException.kt
+++
@@ -1,5 +0,0 @@
-package no.nav.familie.ef.søknad.exception
-
-import no.nav.familie.ef.søknad.service.mellomlagring.Vedlegg
-
-class AttachmentVirusException(val vedlegg: Vedlegg) : AttachmentException(msg = vedlegg.filename)
