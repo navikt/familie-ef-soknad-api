1df50ccc93a5120ce3bf2404c86471eaa5661c60
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-01-22 12:46:16 +0100

    bump spring boot

--- src/main/kotlin/no/nav/familie/ef/søknad/api/filter/CORSResponseFilter.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/filter/CORSResponseFilter.kt
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-@Order(1)
+@Order(0)
 internal class CORSResponseFilter(val corsProperties: CorsProperties) : Filter {
 
     @Throws(IOException::class, ServletException::class)
