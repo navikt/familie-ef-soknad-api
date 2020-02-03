6a79459c9ebf7931523b15c53a29e8a375e95767
Nina Kjekstad <ninakjekstad@Ninas-MacBook-Pro.local>
2020-02-03 15:23:02 +0100

    objektet vi får fra frontend vil også reduseres

--- src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Søker.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Søker.kt
@@ -1,20 +1,10 @@
 package no.nav.familie.ef.søknad.api.dto.søknadsdialog
 
-import no.nav.familie.ef.søknad.api.dto.søknadsdialog.Adresse
-
 data class Søker(
         val adresse: Adresse? = null,
-        val bankkontonummer: String? = null, // Trenger frontend dette?
-        val egenansatt: Boolean? = null, // Frontend trenger ikke dette?
+        val egenansatt: Boolean? = null,
         val fnr: String,
-        val forkortetNavn: String? = null, // Endre hva som returneres fra api - heller fornavn, (mellomnavn,) etternavn? Forkortet navn kan (logisk nok) føre til at man mister deler av navnet
-        val innvandretDato: String? = null,// Trenger frontend dette?
-        val jobbtelefon: String? = null,// Trenger frontend dette?
-        val mobiltelefon: String? = null,
-        val oppholdstillatelse: String? = null,// Trenger frontend dette?
-        val privattelefon: String? = null,
+        val forkortetNavn: String? = null,
         val sivilstand: String? = null,
-        val språk: String? = null,
-        val statsborgerskap: String? = null,
-        val utvandretDato: String? = null// Trenger frontend dette?
+        val statsborgerskap: String? = null
 )
