2c1d381270e30062b2e4d6e9cb93008312ca700b
ma10s <mattis.janitz@nav.no>
2020-02-20 12:58:07 +0100

    (Re)EnableJwtTokenValidation test

--- src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/Application.kt
@@ -1,5 +1,6 @@
 package no.nav.familie.ef.søknad
 
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -8,7 +9,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ef.søknad"])
 @EnableSwagger2
-//@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
+@EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation.swagger.web.ApiResourceController"])
 class Application
 
 fun main(args: Array<String>) {
