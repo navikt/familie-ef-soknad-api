3752a0fff79d800038eebae07fe15d013cd81ed7
ma10s <mattis.janitz@nav.no>
2020-02-21 13:00:30 +0100

    outbound external

--- src/main/kotlin/no/nav/familie/ef/søknad/api/TestController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/TestController.kt
@@ -8,25 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestOperations
-import java.net.URL
+import java.net.URI
 
 
 @RestController
 @RequestMapping(path = ["/api/test"], produces = [APPLICATION_JSON_VALUE])
 @Unprotected
-class TestController(operations: RestOperations) : AbstractRestClient(operations){
+class TestController(operations: RestOperations) : AbstractRestClient(operations) {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     @GetMapping()
     fun sendInn(): String? {
-
-        return URL("https://login.microsoftonline.com/NAVtestB2C.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_idporten_ver1").readText()
-
-//        val uri =
-//                URI("https://login.microsoftonline.com/NAVtestB2C.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_idporten_ver1")
-//        val forEntity = getForEntity<String>(uri)
-//        logger.info(forEntity);
-//         return  forEntity
+        val uri =
+                URI("https://login.microsoftonline.com/NAVtestB2C.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1A_idporten_ver1")
+        val forEntity = getForEntity<String>(uri)
+        logger.info(forEntity);
+        return forEntity
     }
 }
