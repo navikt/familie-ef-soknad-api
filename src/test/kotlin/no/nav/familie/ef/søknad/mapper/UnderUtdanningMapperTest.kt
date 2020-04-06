b1f92a9679ca1c635ec98bb07468181e97146340
ma10s <mattis.janitz@nav.no>
2020-04-06 15:58:19 +0200

    Under utdanning - tester

--- src/test/kotlin/no/nav/familie/ef/søknad/mapper/UnderUtdanningMapperTest.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/mapper/UnderUtdanningMapperTest.kt
@@ -12,17 +12,79 @@ internal class UnderUtdanningMapperTest {
 
     // Når
     private val aktivitet = AktivitetsMapper.map(søknadDto)
-
+    val uderUtdanning = aktivitet.underUtdanning?.verdi
 
     @Test
     fun `Map underUtdanning er mappet `() {
-        Assertions.assertThat(aktivitet.underUtdanning).isNotNull()
+        Assertions.assertThat(uderUtdanning).isNotNull()
     }
 
     @Test
     fun `Map arbeidsforhold sluttdato verdi `() {
-        Assertions.assertThat(aktivitet.underUtdanning?.verdi?.utdanningEtterGrunnskolen?.verdi).isTrue()
+        Assertions.assertThat(uderUtdanning?.utdanningEtterGrunnskolen?.verdi).isTrue()
     }
+
+    @Test
+    fun `Map arbeidsforhold sluttdato label `() {
+        val expected = "Har du tatt utdanning etter grunnskolen"
+        Assertions.assertThat(uderUtdanning?.utdanningEtterGrunnskolen?.label).isEqualTo(expected)
+    }
+
+    @Test
+    fun `Map arbeidsforhold linjeKursGrad verdi `() {
+        Assertions.assertThat(uderUtdanning?.utdanning?.verdi?.linjeKursGrad?.verdi).isEqualTo("Stor kurs grad")
+    }
+
+    @Test
+    fun `Map arbeidsforhold linjeKursGrad label `() {
+        Assertions.assertThat(uderUtdanning?.utdanning?.verdi?.linjeKursGrad?.label).isEqualTo("Linje / kurs / grad")
+    }
+
+    @Test
+    fun `Map arbeidsforhold hvaErMåletMedUtdanningen verdi `() {
+        Assertions.assertThat(uderUtdanning?.hvaErMåletMedUtdanningen?.verdi).isEqualTo("Bli flink")
+    }
+
+    @Test
+    fun `Map arbeidsforhold hvaErMåletMedUtdanningen label `() {
+        Assertions.assertThat(uderUtdanning?.hvaErMåletMedUtdanningen?.label).isEqualTo("Hva er målet med utdanningen?")
+    }
+
+    @Test
+    fun `Map arbeidsforhold offentligEllerPrivat verdi `() {
+        Assertions.assertThat(uderUtdanning?.offentligEllerPrivat?.verdi).isEqualTo("Offentlig")
+    }
+
+    @Test
+    fun `Map arbeidsforhold offentligEllerPrivat label `() {
+        Assertions.assertThat(uderUtdanning?.offentligEllerPrivat?.label).isEqualTo("Er utdanningen privat eller offentlig?")
+    }
+
+    @Test
+    fun `Map arbeidsforhold hvorMyeSkalDuStudere verdi `() {
+        Assertions.assertThat(uderUtdanning?.hvorMyeSkalDuStudere?.verdi).isEqualTo(50)
+    }
+
+    @Test
+    fun `Map arbeidsforhold hvorMyeSkalDuStudere label `() {
+        Assertions.assertThat(uderUtdanning?.hvorMyeSkalDuStudere?.label).isEqualTo("Hvor mye skal du studere?")
+    }
+
+    @Test
+    fun `Map arbeidsforhold skoleUtdanningssted verdi `() {
+        Assertions.assertThat(uderUtdanning?.skoleUtdanningssted?.verdi).isEqualTo("Skoleskolen")
+    }
+
+    @Test
+    fun `Map arbeidsforhold skoleUtdanningssted label `() {
+        Assertions.assertThat(uderUtdanning?.skoleUtdanningssted?.label).isEqualTo("Skole / utdanningssted")
+    }
+
+    @Test
+    fun `Map arbeidsforhold tidligereUtdanninger verdi `() {
+        Assertions.assertThat(uderUtdanning?.tidligereUtdanninger?.verdi).hasSize(1)
+    }
+
 
 
 }
