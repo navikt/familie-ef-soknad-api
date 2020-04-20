4916a566e5dc842bd0ed2ff4f2bd1ca3f3737ed6
ma10s <mattis.janitz@nav.no>
2020-04-20 13:41:38 +0200

    Validering av input (søknad -> fødselsnummer 0g oppslag -> postnummer)

--- src/main/kotlin/no/nav/familie/ef/søknad/api/OppslagController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/OppslagController.kt
@@ -4,9 +4,11 @@ package no.nav.familie.ef.søknad.api
 import no.nav.familie.ef.søknad.api.dto.Søkerinfo
 import no.nav.familie.ef.søknad.service.OppslagService
 import no.nav.familie.ef.søknad.util.InnloggingUtils
+import no.nav.familie.ef.søknad.validering.SjekkGyldigPostnummer
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping(path = [OppslagController.OPPSLAG], produces = [APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = InnloggingUtils.ISSUER, claimMap = ["acr=Level4"])
+@Validated
 class OppslagController(private val oppslagService: OppslagService) {
 
     @GetMapping("/sokerinfo")
@@ -23,7 +26,7 @@ class OppslagController(private val oppslagService: OppslagService) {
     }
 
     @GetMapping("/poststed/{postnummer}")
-    fun postnummer(@PathVariable postnummer: String): ResponseEntity<String> {
+    fun postnummer(@SjekkGyldigPostnummer @PathVariable postnummer: String): ResponseEntity<String> {
         val poststed = oppslagService.hentPoststedFor(postnummer)
         return if (!poststed.isNullOrBlank()) ResponseEntity.ok().body(poststed)
         else ResponseEntity.noContent().build()
