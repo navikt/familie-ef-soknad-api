c61de081b301fee367252d68889c27c90864b994
ma10s <mattis.janitz@nav.no>
2020-06-09 12:52:01 +0200

    Legg til digest på søkerinfo

--- src/main/kotlin/no/nav/familie/ef/søknad/util/IOUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/util/IOUtil.kt
@@ -3,5 +3,12 @@ package no.nav.familie.ef.søknad.util
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.security.MessageDigest
 
 fun getFileAsString(filePath: String) = String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
+
+fun lagDigest(value: Any) = MessageDigest
+        .getInstance("SHA-1")
+        .digest(value.toString().toByteArray())
+        .fold("", { str, it -> str + "%02x".format(it) })
+
---
+++ src/test/kotlin/no/nav/familie/ef/søknad/service/OppslagServiceServiceImplTest.kt
@@ -0,0 +1,75 @@
+package no.nav.familie.ef.søknad.service
+
+import io.mockk.every
+import io.mockk.mockk
+import no.nav.familie.ef.søknad.config.RegelverkConfig
+import no.nav.familie.ef.søknad.integration.TpsInnsynServiceClient
+import no.nav.familie.ef.søknad.integration.dto.NavnDto
+import no.nav.familie.ef.søknad.integration.dto.PersoninfoDto
+import no.nav.familie.ef.søknad.integration.dto.RelasjonDto
+import no.nav.familie.ef.søknad.mock.TpsInnsynMockController
+import no.nav.familie.kontrakter.felles.objectMapper
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+internal class OppslagServiceServiceImplTest {
+
+
+    val client: TpsInnsynServiceClient = mockk()
+    val tpsInnsynMockController = TpsInnsynMockController()
+    val regelverkConfig: RegelverkConfig = mockk()
+
+    private val oppslagServiceService: OppslagServiceServiceImpl = OppslagServiceServiceImpl(client, mockk(), regelverkConfig)
+
+    @Before
+    fun setUp() {
+        mockRegelverksConfig()
+        mockHentBarn()
+        mockHentPersonInfo()
+    }
+
+    @Test
+    fun `Lik søkerInfo skal ha lik hash`() {
+        val søkerinfo = oppslagServiceService.hentSøkerinfo()
+        val søkerinfo2 = oppslagServiceService.hentSøkerinfo()
+        assertEquals(søkerinfo.hash, søkerinfo2.hash)
+    }
+
+    @Test
+    fun `SøkerInfo med ulike navn skal ikke ha lik hash`() {
+        val søkerinfo = oppslagServiceService.hentSøkerinfo()
+        mockHentPersonInfo("Et annet navn")
+        val søkerinfo2 = oppslagServiceService.hentSøkerinfo()
+        assertNotEquals(søkerinfo.hash, søkerinfo2.hash)
+    }
+
+    @Test
+    fun `SøkerInfo med endring i barn skal ikke ha lik hash`() {
+        val søkerinfo = oppslagServiceService.hentSøkerinfo()
+        mockHentBarn("AnnetBarnenavn")
+        val søkerinfo2 = oppslagServiceService.hentSøkerinfo()
+        assertNotEquals(søkerinfo.hash, søkerinfo2.hash)
+    }
+
+    private fun mockRegelverksConfig() {
+        every { regelverkConfig.alder } returns RegelverkConfig.Alder(18)
+    }
+
+    private fun mockHentBarn(navn: String = "Ola") {
+        val barnFraTpsMocked = tpsInnsynMockController.barnFraTpsMocked()
+        val collectionType =
+                objectMapper.getTypeFactory().constructCollectionType(List::class.java, RelasjonDto::class.java)
+        val barnListDto: List<RelasjonDto> = objectMapper.readValue(barnFraTpsMocked, collectionType)
+        val copyAvBarn = barnListDto[0].copy(forkortetNavn = navn)
+        every { client.hentBarn() } returns (listOf(copyAvBarn))
+    }
+
+    private fun mockHentPersonInfo(nyttNavn: String = "TestNavn") {
+        val søkerinfoFraTpsMocked = tpsInnsynMockController.søkerinfoFraTpsMocked()
+        val personInfoDto: PersoninfoDto = objectMapper.readValue(søkerinfoFraTpsMocked, PersoninfoDto::class.java)
+        every { client.hentPersoninfo() } returns (personInfoDto.copy(navn = NavnDto(nyttNavn)))
+    }
+
+}
