521c108f40402e8c3dabbe201f59ff689eb384db
Mattis Janitz <mattis.janitz@nav.no>
2020-06-10 11:50:26 +0200

    Legg til digest på søkerinfo (#149)

--- src/main/kotlin/no/nav/familie/ef/søknad/api/dto/Søkerinfo.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/dto/Søkerinfo.kt
@@ -2,6 +2,11 @@ package no.nav.familie.ef.søknad.api.dto
 
 import no.nav.familie.ef.søknad.api.dto.tps.Barn
 import no.nav.familie.ef.søknad.api.dto.tps.Person
+import no.nav.familie.ef.søknad.util.lagDigest
 
 data class Søkerinfo(val søker: Person,
-                     val barn: List<Barn>)
+                     val barn: List<Barn>) {
+
+    val hash: String = lagDigest(this)
+
+}
