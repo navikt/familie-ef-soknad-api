d4e696d079a3569e842211b3d349ce4976cda605
Johan Blomgren <johan.blomgren@nav.no>
2020-06-17 10:14:25 +0200

    Lagt til mellomlager i testmocks

--- src/test/kotlin/no/nav/familie/ef/søknad/mock/DokumentConfiguration.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/mock/DokumentConfiguration.kt
@@ -1,6 +1,7 @@
 package no.nav.familie.ef.søknad.mock
 
 import no.nav.familie.ef.søknad.service.DokumentService
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -10,12 +11,13 @@ import org.springframework.context.annotation.Profile
 @Configuration
 class DokumentConfiguration {
 
-    @Bean
-    fun dokumenter() : Map<String, ByteArray> = mutableMapOf()
+    @Bean("dokumentlager")
+    fun dokumenter(): Map<String, ByteArray> = mutableMapOf()
 
     @Bean
     @Primary
-    fun dokumentService(dokumenter: Map<String, ByteArray>): DokumentService = object : DokumentService {
-        override fun hentVedlegg(vedleggsId: String): ByteArray = dokumenter[vedleggsId] ?: error("Finner ikke vedlegg")
-    }
+    fun dokumentService(@Qualifier("dokumentlager") dokumenter: Map<String, ByteArray>): DokumentService =
+            object : DokumentService {
+                override fun hentVedlegg(vedleggsId: String): ByteArray = dokumenter[vedleggsId] ?: error("Finner ikke vedlegg")
+            }
 }
--- src/test/kotlin/no/nav/familie/ef/søknad/mock/DokumentController.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/mock/DokumentController.kt
@@ -1,6 +1,13 @@
 package no.nav.familie.ef.søknad.mock
 
+import com.amazonaws.services.s3.model.AmazonS3Exception
+import no.nav.familie.kontrakter.felles.objectMapper
 import no.nav.security.token.support.core.api.Unprotected
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -8,12 +15,23 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
+@Profile("mock-dokument")
+@Suppress("SpringJavaInjectionPointsAutowiringInspection") //dokumentlager er definiert i DokumentConfiguration
 @RestController
 @RequestMapping(path = ["/dokumentmock/"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @Unprotected
-class DokumentController(private val dokumenter: MutableMap<String, ByteArray>) {
+class DokumentController(@Qualifier("dokumentlager") private val dokumenter: MutableMap<String, ByteArray>,
+                         private val contextHolder: TokenValidationContextHolder) {
 
-    @PostMapping(path = ["{bucket}"],
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    private val mellomlager = mutableMapOf<String, String>()
+
+    fun TokenValidationContextHolder.hentFnr(): String {
+        return this.tokenValidationContext.getJwtToken("selvbetjening").subject
+    }
+
+    @PostMapping(path = ["dokument/{bucket}"],
                  consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
                  produces = [MediaType.APPLICATION_JSON_VALUE])
     fun addAttachment(@PathVariable("bucket") bucket: String,
@@ -29,14 +47,68 @@ class DokumentController(private val dokumenter: MutableMap<String, ByteArray>) 
             throw IllegalArgumentException(HttpStatus.PAYLOAD_TOO_LARGE.toString())
         }
 
-        val uuid = UUID.randomUUID().toString()
-        try {
+        return try {
+            val uuid = UUID.randomUUID().toString()
             dokumenter[uuid] = bytes
+            ResponseEntity.ok(mapOf("dokumentId" to uuid, "filnavn" to multipartFile.name))
         } catch (e: RuntimeException) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
+    }
+
+    @PostMapping("mellomlager",
+                 consumes = [MediaType.APPLICATION_JSON_VALUE],
+                 produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun mellomlagreSøknad(@RequestBody(required = true) søknad: String): ResponseEntity<Unit> {
+        log.info("Mellomlagrer søknad om overgangsstønad")
+
+        validerGyldigJson(søknad)
+        val directory = contextHolder.hentFnr()
+
+        try {
+            mellomlager[directory] = søknad
+        } catch (e: RuntimeException) {
+            log.warn("Kunne ikke mellomlagre overgangsstønad for $directory", e)
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
         }
 
-        return ResponseEntity.ok(mapOf("dokumentId" to uuid, "filnavn" to multipartFile.name))
+        return ResponseEntity.status(HttpStatus.CREATED).build()
+    }
+
+    @GetMapping("mellomlager", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun hentMellomlagretSøknad(): ResponseEntity<String> {
+        val directory = contextHolder.hentFnr()
+        return try {
+            ResponseEntity.ok(mellomlager[directory])
+        } catch (e: RuntimeException) {
+            if (e is AmazonS3Exception && e.statusCode == 404) {
+                ResponseEntity.noContent().build()
+            } else {
+                log.info("Noe gikk galt", e)
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            }
+        }
+    }
+
+    @DeleteMapping("mellomlager", produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun slettMellomlagretSøknad(): ResponseEntity<String> {
+        val directory = contextHolder.hentFnr()
+        return try {
+            log.debug("Sletter mellomlagret overgangsstønad")
+            mellomlager.remove(directory)
+            ResponseEntity.status(HttpStatus.NO_CONTENT).build()
+        } catch (e: RuntimeException) {
+            log.warn("Kunne ikke slette mellomlagret overgangsstønad for $directory", e)
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
+    }
+
+    private fun validerGyldigJson(verdi: String) {
+        try {
+            objectMapper.readTree(verdi);
+        } catch (e: Exception) {
+            error("Forsøker å mellomlagre søknad som ikke er gyldig json-verdi")
+        }
     }
 
     companion object {
