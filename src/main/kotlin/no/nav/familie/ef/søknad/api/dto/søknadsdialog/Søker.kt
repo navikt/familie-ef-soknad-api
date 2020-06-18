58298c114feb703e0ea5f797354e977cd7c726b3
charliemidtlyng <charlie.midtlyng@bekk.no>
2020-06-18 14:39:30 +0200

    Map kontakttelefon fra søknadsdialogen

--- src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Søker.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Søker.kt
@@ -9,7 +9,8 @@ data class Søker(val adresse: Adresse,
                  val forkortetNavn: String,
                  val sivilstand: String,
                  val statsborgerskap: String,
-                 val telefonnummer: String?)
+                 val telefonnummer: String?,
+                 val kontakttelefon: String? = null)
 
 // TODO Ikke fra UI
 // telefonnummer?
