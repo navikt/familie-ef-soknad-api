2697f47ef43f958d226032780443d668ce096c13
bent-lorentzen <54444412+bent-lorentzen@users.noreply.github.com>
2020-06-19 10:36:34 +0200

    Oppdatert kontrakt. (#164)

--- src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Situasjon.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/dto/søknadsdialog/Situasjon.kt
@@ -1,15 +1,13 @@
 package no.nav.familie.ef.søknad.api.dto.søknadsdialog
 
-data class Situasjon(
-        val datoOppstartJobb: DatoFelt? = null,
-        val datoOppstartUtdanning: DatoFelt? = null,
-        val gjelderDetteDeg: ListFelt<String>,
-        val sagtOppEllerRedusertStilling: TekstFelt? = null,
-        val datoSagtOppEllerRedusertStilling: DatoFelt? = null,
-        val begrunnelseSagtOppEllerRedusertStilling: TekstFelt? = null,
-        val søknadsdato: DatoFelt?,
-        val søkerFraBestemtMåned: TekstFelt
-)
+data class Situasjon(val datoOppstartJobb: DatoFelt? = null,
+                     val datoOppstartUtdanning: DatoFelt? = null,
+                     val gjelderDetteDeg: ListFelt<String>,
+                     val sagtOppEllerRedusertStilling: TekstFelt? = null,
+                     val datoSagtOppEllerRedusertStilling: DatoFelt? = null,
+                     val begrunnelseSagtOppEllerRedusertStilling: TekstFelt? = null,
+                     val søknadsdato: DatoFelt?,
+                     val søkerFraBestemtMåned: BooleanFelt)
 
 //export interface IDinSituasjon {
 //    gjelderDetteDeg: ISpørsmålListeFelt;
