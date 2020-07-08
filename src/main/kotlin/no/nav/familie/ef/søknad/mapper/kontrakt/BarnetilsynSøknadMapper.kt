cf0a0053c40de65ef7e84001c2138c9e265d8d65
ma10s <mattis.janitz@nav.no>
2020-07-08 14:25:32 +0200

    Legg til innsendngsdetaljer

--- src/main/kotlin/no/nav/familie/ef/søknad/mapper/kontrakt/BarnetilsynSøknadMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/mapper/kontrakt/BarnetilsynSøknadMapper.kt
@@ -6,10 +6,7 @@ import no.nav.familie.ef.søknad.integration.BarnetilsynRequestData
 import no.nav.familie.ef.søknad.mapper.DokumentasjonWrapper
 import no.nav.familie.ef.søknad.mapper.lagDokumentasjonWrapper
 import no.nav.familie.ef.søknad.service.DokumentService
-import no.nav.familie.kontrakter.ef.søknad.Barn
-import no.nav.familie.kontrakter.ef.søknad.Personalia
-import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
-import no.nav.familie.kontrakter.ef.søknad.Vedlegg
+import no.nav.familie.kontrakter.ef.søknad.*
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
@@ -23,10 +20,18 @@ class BarnetilsynSøknadMapper(private val dokumentServiceService: DokumentServi
         val vedleggData: Map<String, ByteArray> = hentDokumenter(barnetilsynDto.dokumentasjonsbehov)
         val vedlegg: Map<String, DokumentasjonWrapper> = lagDokumentasjonWrapper(barnetilsynDto.dokumentasjonsbehov)
 
-        val barnetilsynSøknad = BarnetilsynSøknadKontrakt(barnetilsynDto.person.søker.tilSøknadsFelt(),
+        val barnetilsynSøknad = BarnetilsynSøknadKontrakt(innsendingsDetaljer(innsendingMottatt),
+                                                          barnetilsynDto.person.søker.tilSøknadsFelt(),
                                                           barnetilsynDto.person.barn.tilSøknadsfelt(vedlegg))
+
         return BarnetilsynRequestData(SøknadBarnetilsyn(barnetilsynSøknad, vedlegg.values.map { it.vedlegg }.flatten()),
                                       vedleggData)
+    }
+
+    private fun innsendingsDetaljer(innsendingMottatt: LocalDateTime): Søknadsfelt<Innsendingsdetaljer> {
+        return Søknadsfelt("Innsendingsdetaljer",
+                           Innsendingsdetaljer(Søknadsfelt("Dato mottatt",
+                                                           innsendingMottatt)))
     }
 
     private fun hentDokumenter(dokumentasjonsbehov: List<Dokumentasjonsbehov>): Map<String, ByteArray> {
@@ -42,7 +47,9 @@ class BarnetilsynSøknadMapper(private val dokumentServiceService: DokumentServi
 
 // TODO flyttes til kontrakter
 data class SøknadBarnetilsyn(val søknad: BarnetilsynSøknadKontrakt, val vedlegg: List<Vedlegg>)
-data class BarnetilsynSøknadKontrakt(val personalia: Søknadsfelt<Personalia>,
-                                     val barn: Søknadsfelt<List<Barn>>)
+data class BarnetilsynSøknadKontrakt(
+        val innsendingsdetaljer: Søknadsfelt<Innsendingsdetaljer>,
+        val personalia: Søknadsfelt<Personalia>,
+        val barn: Søknadsfelt<List<Barn>>)
 
 
