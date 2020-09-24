2d70d89a103ca47b879edfbd2e17577ee3a84aba
ma10s <mattis.janitz@nav.no>
2020-09-24 08:09:36 +0200

    PDL - tmp, work in progress

--- src/main/kotlin/no/nav/familie/ef/søknad/service/OppslagServiceServiceImpl.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/service/OppslagServiceServiceImpl.kt
@@ -3,8 +3,6 @@ package no.nav.familie.ef.søknad.service
 import no.nav.familie.ef.søknad.api.dto.Søkerinfo
 import no.nav.familie.ef.søknad.config.RegelverkConfig
 import no.nav.familie.ef.søknad.integration.TpsInnsynServiceClient
-import no.nav.familie.ef.søknad.integration.dto.PersoninfoDto
-import no.nav.familie.ef.søknad.integration.dto.RelasjonDto
 import no.nav.familie.ef.søknad.mapper.SøkerinfoMapper
 import org.apache.commons.logging.LogFactory
 import org.slf4j.LoggerFactory
@@ -24,28 +22,11 @@ internal class OppslagServiceServiceImpl(private val client: TpsInnsynServiceCli
     override fun hentSøkerinfo(): Søkerinfo {
         val personinfoDto = client.hentPersoninfo()
         val barn = client.hentBarn()
-
-        logSecure(personinfoDto, barn)
-
+        secureLogger.info("Personinfo: ${personinfoDto.ident}, Barn alder/samme adresse : ${
+            barn.map { Pair(it.alder, it.harSammeAdresse) }
+        },  ")
         val aktuelleBarn = barn.filter { erIAktuellAlder(it.fødselsdato) }
-
-        if (aktuelleBarn.size < barn.size) {
-            logSecure(personinfoDto, aktuelleBarn, "Aktuelle barn")
-        }
         return søkerinfoMapper.mapTilSøkerinfo(personinfoDto, aktuelleBarn)
-    }
-
-    private fun logSecure(personinfoDto: PersoninfoDto,
-                          barn: List<RelasjonDto>, tekst: String = "Barn alder/samme adresse") {
-        try {
-            val logTekst = "Personinfo: ${personinfoDto.ident}, $tekst : ${
-                barn.map { Pair(it.alder, it.harSammeAdresse) }
-            },  "
-            secureLogger.info(logTekst)
-        } catch (e: Exception) {
-            // svelg denne, kun logging
-        }
-
     }
 
     fun erIAktuellAlder(fødselsdato: LocalDate?): Boolean {
