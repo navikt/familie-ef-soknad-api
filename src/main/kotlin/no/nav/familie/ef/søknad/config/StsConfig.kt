c35e64f947317f66d307167e4b357d653496bde8
Johan Blomgren <johan.blomgren@nav.no>
2020-10-23 14:18:11 +0200

    Slettet stsconfig...

--- src/main/kotlin/no/nav/familie/ef/søknad/config/StsConfig.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.familie.ef.søknad.config
-
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Configuration
-import java.net.URI
-
-@Configuration
-class StsConfig(@Value("\${STS_URL}") val uri: URI,
-                @Value("\${sts.passord}") val passord: String)
