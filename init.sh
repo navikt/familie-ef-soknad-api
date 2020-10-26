c4124d1350a13a30a74b2481317f89580aed2562
Johan Blomgren <johan.blomgren@nav.no>
2020-10-26 11:24:55 +0100

    Kommentar på credentials

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+# CREDENTIAL_PASSWORD og CREDENTIAL_USERNAME brukes til StsRestClient
 export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/srvfam-ef-soknad-api/password)
 export CREDENTIAL_USERNAME=$(cat /var/run/secrets/srvfam-ef-soknad-api/username)
 echo "- exported CREDENTIAL_USERNAME og CREDENTIAL_PASSWORD for familie-ef-soknad-api "
