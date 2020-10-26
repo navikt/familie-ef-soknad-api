8049e6fe97c05f8af777b6154c871738827a19c0
Johan Blomgren <johan.blomgren@nav.no>
2020-10-26 08:40:45 +0100

    Lagt til gruppetilgang-servicebruker

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/srvfam-ef-soknad-api-sbs/password)
-export CREDENTIAL_USERNAME=$(cat /var/run/secrets/srvfam-ef-soknad-api-sbs/username)
+export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/srvfam-ef-soknad-api/password)
+export CREDENTIAL_USERNAME=$(cat /var/run/secrets/srvfam-ef-soknad-api/username)
 echo "- exported CREDENTIAL_USERNAME og CREDENTIAL_PASSWORD for familie-ef-soknad-api "
