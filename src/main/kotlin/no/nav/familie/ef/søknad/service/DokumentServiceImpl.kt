a8c8f655bb1a0d6cb501c595aa6eb41ea84c33b4
Johan Blomgren <blommish@users.noreply.github.com>
2020-11-10 15:19:00 +0100

    Gå over til å bruke dokument-gcp (#268)
    * Lagt til henting av dokument fra gcp
    
    * GCP kaster 404 hvis vedlegget ikke finnes
    
    * Featuretoggler henting av vedlegg
    
    * Bruker *.nav.no ingress for dokument i gcp

--- src/main/kotlin/no/nav/familie/ef/søknad/service/DokumentServiceImpl.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/service/DokumentServiceImpl.kt
@@ -1,13 +1,33 @@
 package no.nav.familie.ef.søknad.service
 
+import no.nav.familie.ef.søknad.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.søknad.integration.FamilieDokumentClient
+import no.nav.familie.ef.søknad.integration.FamilieDokumentSbsClient
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.web.client.HttpClientErrorException
 
 @Service
-internal class DokumentServiceImpl(private val client: FamilieDokumentClient) : DokumentService {
+internal class DokumentServiceImpl(private val client: FamilieDokumentClient,
+                                   private val featureToggleService: FeatureToggleService,
+                                   private val sbsClient: FamilieDokumentSbsClient) : DokumentService {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun hentVedlegg(vedleggsId: String): ByteArray {
-        return client.hentVedlegg(vedleggsId)
+        return if (featureToggleService.isEnabled("familie.ef.soknad.dokument-gcp")) {
+            try {
+                logger.info("Henter vedlegg $vedleggsId fra gcp")
+                val hentVedlegg = client.hentVedlegg(vedleggsId)
+                logger.info("Fant vedlegg=$vedleggsId i gcp")
+                hentVedlegg
+            } catch (e: HttpClientErrorException.NotFound) {
+                logger.warn("Finner ikke vedlegg=$vedleggsId i gcp - prøver å hente fra sbs")
+                sbsClient.hentVedlegg(vedleggsId)
+            }
+        } else {
+            sbsClient.hentVedlegg(vedleggsId)
+        }
     }
 
 }
