b865f1954f9988698816b9f4da7f982e8b6dc96a
ma10s <mattis.janitz@nav.no>
2021-04-29 16:51:03 +0200

    Fjern litt unødvendig logging

--- src/main/kotlin/no/nav/familie/ef/søknad/service/DokumentServiceImpl.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/service/DokumentServiceImpl.kt
@@ -3,7 +3,6 @@ package no.nav.familie.ef.søknad.service
 import no.nav.familie.ef.søknad.integration.FamilieDokumentClient
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import org.springframework.web.client.HttpClientErrorException
 
 @Service
 internal class DokumentServiceImpl(private val client: FamilieDokumentClient) : DokumentService {
@@ -11,15 +10,8 @@ internal class DokumentServiceImpl(private val client: FamilieDokumentClient) : 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun hentVedlegg(vedleggsId: String): ByteArray {
-        try {
-            logger.info("Henter vedlegg $vedleggsId fra gcp")
-            val hentVedlegg = client.hentVedlegg(vedleggsId)
-            logger.info("Fant vedlegg=$vedleggsId i gcp")
-            return hentVedlegg
-        } catch (e: HttpClientErrorException.NotFound) {
-            logger.warn("Finner ikke vedlegg=$vedleggsId i gcp")
-            throw (e)
-        }
+        logger.info("Henter vedlegg $vedleggsId fra gcp")
+        return client.hentVedlegg(vedleggsId)
     }
 }
 
