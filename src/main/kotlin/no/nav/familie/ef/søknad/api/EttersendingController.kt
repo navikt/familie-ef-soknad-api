1e574b52045b119e69e8b9823caa03f51153fedb
eirarset <eirik.arseth@nav.no>
2021-07-20 10:21:44 +0200

    Setter en featuretoggle foran endpeunktet for ettersending

--- src/main/kotlin/no/nav/familie/ef/søknad/api/EttersendingController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/EttersendingController.kt
@@ -3,10 +3,12 @@ package no.nav.familie.ef.søknad.mock
 import no.nav.familie.ef.søknad.api.ApiFeil
 import no.nav.familie.ef.søknad.api.dto.Kvittering
 import no.nav.familie.ef.søknad.api.dto.søknadsdialog.EttersendingDto
+import no.nav.familie.ef.søknad.featuretoggle.FeatureToggleService
+import no.nav.familie.ef.søknad.featuretoggle.enabledEllersHttp403
+import no.nav.familie.ef.søknad.service.EttersendingService
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.HttpStatus
-import no.nav.familie.ef.søknad.service.EttersendingService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,17 +18,18 @@ import java.time.LocalDateTime
 @RestController
 @RequestMapping(path = ["/api/ettersending"])
 @ProtectedWithClaims(issuer = EksternBrukerUtils.ISSUER, claimMap = ["acr=Level4"])
-class EttersendingController (val ettersendingService: EttersendingService) {
+class EttersendingController(val ettersendingService: EttersendingService, val featureToggleService: FeatureToggleService) {
 
     @PostMapping
     fun postEttersending(@RequestBody ettersending: EttersendingDto): Kvittering {
-        if (!EksternBrukerUtils.personIdentErLikInnloggetBruker(ettersending.person.søker.fnr)) {
-            throw ApiFeil("Fnr fra token matcher ikke fnr på søknaden", HttpStatus.FORBIDDEN)
+        return featureToggleService.enabledEllersHttp403("familie.ef.soknad.api.ettersending") {
+            if (!EksternBrukerUtils.personIdentErLikInnloggetBruker(ettersending.person.søker.fnr)) {
+                throw ApiFeil("Fnr fra token matcher ikke fnr på søknaden", HttpStatus.FORBIDDEN)
+            }
+            val innsendingMottatt = LocalDateTime.now()
+            ettersendingService.sendInn(ettersending, innsendingMottatt)
+            Kvittering("ok", mottattDato = innsendingMottatt)
         }
-        val innsendingMottatt = LocalDateTime.now()
-        ettersendingService.sendInn(ettersending, innsendingMottatt)
-        return Kvittering("ok", mottattDato = innsendingMottatt)
-
     }
 
 }
