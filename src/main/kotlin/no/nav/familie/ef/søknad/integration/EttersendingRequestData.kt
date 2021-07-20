a00a3fd18ddee44c5c3621dd13d6d9191fa0a855
Simen <simentengs@gmail.com>
2021-07-20 09:59:18 +0200

    fikset type på EttersendingRequestData

--- src/main/kotlin/no/nav/familie/ef/søknad/integration/EttersendingRequestData.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/EttersendingRequestData.kt
@@ -2,7 +2,7 @@ package no.nav.familie.ef.søknad.integration
 
 import no.nav.familie.kontrakter.ef.ettersending.EttersendingMedVedlegg
 
-data class EttersendingRequestData<T>(
-    val ettersendingMedVedlegg: EttersendingMedVedlegg<T>,
+data class EttersendingRequestData<Ettersending>(
+    val ettersendingMedVedlegg: EttersendingMedVedlegg,
     val vedlegg: Map<String, ByteArray>
 )
