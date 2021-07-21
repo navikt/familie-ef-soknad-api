dfb925de37a88efeef073f73173dea5dcb7a7487
Simen <simentengs@gmail.com>
2021-07-21 12:15:19 +0200

    oppdatert test-dto

--- src/test/kotlin/no/nav/familie/ef/søknad/mock/EttersendingDto.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/mock/EttersendingDto.kt
@@ -1,3 +1,11 @@
 package no.nav.familie.ef.søknad.mock
 
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.familie.ef.søknad.api.dto.ettersending.EttersendingDto
+import no.nav.familie.kontrakter.felles.objectMapper
+import java.io.File
 
+
+fun EttersendingForSøknadDto(): EttersendingDto = objectMapper.readValue(File("src/test/resources/ettersendingDtoForSøknad.json"), EttersendingDto::class.java)
+
+fun EttersendingUtenSøknadDto(): EttersendingDto = objectMapper.readValue(File("src/test/resources/ettersendingDtoUtenSøknad.json"), EttersendingDto::class.java)
