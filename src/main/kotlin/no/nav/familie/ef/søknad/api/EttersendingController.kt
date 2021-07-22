316357d45db808ec5abd1411135ce8cefe1a7992
Simen <simentengs@gmail.com>
2021-07-22 14:25:26 +0200

    formatering

--- src/main/kotlin/no/nav/familie/ef/søknad/api/EttersendingController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/EttersendingController.kt
@@ -18,9 +18,8 @@ import java.time.LocalDateTime
 @RequestMapping(path = ["/api/ettersending"])
 @ProtectedWithClaims(issuer = EksternBrukerUtils.ISSUER, claimMap = ["acr=Level4"])
 
-class EttersendingController(
-        val ettersendingService: EttersendingService,
-        val featureToggleService: FeatureToggleService
+class EttersendingController(val ettersendingService: EttersendingService,
+                             val featureToggleService: FeatureToggleService
 ) {
 
     @PostMapping
