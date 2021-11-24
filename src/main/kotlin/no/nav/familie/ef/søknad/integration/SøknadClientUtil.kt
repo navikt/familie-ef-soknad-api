c6bc2ab1e487dac6befc93071d8c9b0f330a3c7e
Viktor <viktor.grondalen.solberg@nav.no>
2021-11-24 18:01:36 +0100

    typo

--- src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClientUtil.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/SøknadClientUtil.kt
@@ -6,7 +6,7 @@ import java.time.LocalDate
 object SøknadClientUtil {
 
     fun filtrerVekkEldreDokumentasjonsbehov(søknader: List<SøknadMedDokumentasjonsbehovDto>): List<SøknadMedDokumentasjonsbehovDto> {
-        val FIRE_UKER: Long = 4 * 7; /* dager */
-        return søknader.filter { LocalDate.now().toEpochDay() - it.søknadDato.toEpochDay() < FIRE_UKER }
+        val fireUker: Long = 4 * 7; /* dager */
+        return søknader.filter { LocalDate.now().toEpochDay() - it.søknadDato.toEpochDay() < fireUker }
     }
 }
