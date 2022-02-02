07b6a88af6b133cbb878132948b87eb8814c4ac3
ma10s <mattis.janitz@nav.no>
2022-02-02 10:39:27 +0100

    Fjerner mer unødvendig logging

--- src/main/kotlin/no/nav/familie/ef/søknad/mapper/Mapper.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/mapper/Mapper.kt
@@ -4,15 +4,11 @@ import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
 import org.slf4j.LoggerFactory
 
 abstract class Mapper<T, R>(private val rootLabel: Språktekster) {
+
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun map(data: T): Søknadsfelt<R> {
-        try {
-            return Søknadsfelt(rootLabel.hentTekst(), mapDto(data))
-        } catch (e: Exception) {
-            logger.error("Feil ved mapping av ${javaClass.simpleName}")
-            throw e
-        }
+        return Søknadsfelt(rootLabel.hentTekst(), mapDto(data))
     }
 
     protected abstract fun mapDto(data: T): R
--- src/main/kotlin/no/nav/familie/ef/søknad/mapper/MapperMedVedlegg.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/mapper/MapperMedVedlegg.kt
@@ -4,14 +4,10 @@ import no.nav.familie.kontrakter.ef.søknad.Søknadsfelt
 import org.slf4j.LoggerFactory
 
 abstract class MapperMedVedlegg<T, R>(private val rootLabel: Språktekster) {
+
     private val logger = LoggerFactory.getLogger(this::class.java)
     fun map(data: T, vedlegg: Map<String, DokumentasjonWrapper>): Søknadsfelt<R> {
-        try {
-            return Søknadsfelt(rootLabel.hentTekst(), mapDto(data, vedlegg))
-        } catch (e: Exception) {
-            logger.error("Feil ved mapping av ${javaClass.simpleName}")
-            throw e
-        }
+        return Søknadsfelt(rootLabel.hentTekst(), mapDto(data, vedlegg))
     }
 
     protected abstract fun mapDto(data: T, vedlegg: Map<String, DokumentasjonWrapper>): R
