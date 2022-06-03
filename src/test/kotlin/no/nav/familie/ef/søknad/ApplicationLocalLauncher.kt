e4b5ddc1fdde14fcc97b469297fa96e5f27e6812
ma10s <mattis.janitz@nav.no>
2022-06-03 14:51:45 +0200

    WIP

--- src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
@@ -2,14 +2,13 @@ package no.nav.familie.ef.søknad
 
 import no.nav.familie.ef.søknad.config.ApplicationConfig
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import no.nav.security.token.support.test.spring.TokenGeneratorConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
 
 @SpringBootApplication(scanBasePackages = ["no.nav.familie.ef.søknad"], exclude = [ErrorMvcAutoConfiguration::class])
-@Import(ApplicationConfig::class, TokenGeneratorConfiguration::class)
+@Import(ApplicationConfig::class)
 @EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
 class ApplicationLocalLauncher
 
