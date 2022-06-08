34e2094acb7290d8c2f7417846304654b65c30dc
ma10s <mattis.janitz@nav.no>
2022-06-08 12:51:15 +0200

    Vil importere config fra ApplicationLocalTestLauncher og kun sette @EnableMockOAuth2Server i ApplicationLocalLauncher

--- src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
@@ -1,29 +1,24 @@
 package no.nav.familie.ef.søknad
 
-import no.nav.familie.ef.søknad.config.ApplicationConfig
-import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import no.nav.familie.ef.søknad.integrationTest.ApplicationLocalTestLauncher
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Import
 
-@SpringBootApplication(scanBasePackages = ["no.nav.familie.ef.søknad"], exclude = [ErrorMvcAutoConfiguration::class])
-@Import(ApplicationConfig::class)
-@EnableJwtTokenValidation(ignore = ["org.springframework", "org.springdoc"])
+@Import(ApplicationLocalTestLauncher::class)
 @EnableMockOAuth2Server
 class ApplicationLocalLauncher
 
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationLocalLauncher::class.java)
-        .profiles(
-            "local",
-            "mock-kodeverk",
-            "mock-dokument",
-            "mock-pdl",
-            "mock-pdlApp2AppClient",
-            "mock-mottak",
-            "mock-integrasjoner"
-        )
-        .run(*args)
+            .profiles(
+                    "local",
+                    "mock-kodeverk",
+                    "mock-dokument",
+                    "mock-pdl",
+                    "mock-pdlApp2AppClient",
+                    "mock-mottak",
+                    "mock-integrasjoner"
+            )
+            .run(*args)
 }
