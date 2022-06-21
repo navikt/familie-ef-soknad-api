03a03cfc106ae9b56c4d10acb8782ff67867ccff
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-21 10:26:37 +0200

    Setter mockOauth til fixed port for lokal kjøring for å bruke den samme porten som familie-dokument og kunne kalle dokument lokalt

--- src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
@@ -7,16 +7,22 @@ import org.springframework.boot.builder.SpringApplicationBuilder
 @EnableMockOAuth2Server
 class ApplicationLocalLauncher : ApplicationLocalTestLauncher()
 
+/**
+ * Denne settes til en fixed port for å kunne bruke samme port som familie-dokument
+ */
+private val mockOauth2ServerPort: String = "11588"
+
 fun main(args: Array<String>) {
     SpringApplicationBuilder(ApplicationLocalLauncher::class.java)
-            .profiles(
-                    "local",
-                    "mock-kodeverk",
-                    "mock-dokument",
-                    "mock-pdl",
-                    "mock-pdlApp2AppClient",
-                    "mock-mottak",
-                    "mock-integrasjoner"
-            )
-            .run(*args)
+        .profiles(
+            "local",
+            "mock-kodeverk",
+            "mock-dokument",
+            "mock-pdl",
+            "mock-pdlApp2AppClient",
+            "mock-mottak",
+            "mock-integrasjoner"
+        )
+        .properties(mapOf("mock-oauth2-server.port" to mockOauth2ServerPort))
+        .run(*args)
 }
