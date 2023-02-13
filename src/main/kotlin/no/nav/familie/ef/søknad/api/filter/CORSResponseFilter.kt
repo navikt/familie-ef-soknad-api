2154ba5d74fcacfd9d2549c9bde79569b4af1629
Johan Blomgren <blommish@users.noreply.github.com>
2023-02-13 09:50:27 +0100

    Erstatter javax.servlet med jakarta.servlet

--- src/main/kotlin/no/nav/familie/ef/søknad/api/filter/CORSResponseFilter.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/filter/CORSResponseFilter.kt
@@ -1,16 +1,16 @@
 package no.nav.familie.ef.søknad.api.filter
 
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import no.nav.familie.ef.søknad.config.CorsProperties
 import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import java.io.IOException
-import javax.servlet.Filter
-import javax.servlet.FilterChain
-import javax.servlet.ServletException
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
 
 @Component
 @Order(0)
