eab1dddf99dc938859a02604de1ba9ba3c13fb2f
throndi <56258085+throndi@users.noreply.github.com>
2023-05-16 14:46:08 +0200

    Skal legge ved behandlingsnummer fra behandlingskatalogen i en header ved kall til PDL. Datatyper for PdlExtensions og PdlWarning, og logging av warnings.

--- src/main/kotlin/no/nav/familie/ef/søknad/integration/dto/pdl/PdlPerson.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/integration/dto/pdl/PdlPerson.kt
@@ -6,23 +6,29 @@ import java.time.LocalDate
 data class PdlResponse<T>(
     val data: T,
     val errors: List<PdlError>?,
+    val extensions: PdlExtensions?,
 ) {
 
     fun harFeil(): Boolean {
         return errors != null && errors.isNotEmpty()
     }
-
+    fun harAdvarsel(): Boolean {
+        return !extensions?.warnings.isNullOrEmpty()
+    }
     fun errorMessages(): String {
         return errors?.joinToString { it -> it.message } ?: ""
     }
 }
 
+data class PdlExtensions(val warnings: List<PdlWarning>?)
+data class PdlWarning(val details: Any?, val id: String?, val message: String?, val query: String?)
+
 data class PdlError(
     val message: String,
-    val extensions: PdlExtensions?,
+    val extensions: PdlErrorExtensions?,
 )
 
-data class PdlExtensions(val code: String?) {
+data class PdlErrorExtensions(val code: String?) {
 
     fun notFound() = code == "not_found"
 }
@@ -31,10 +37,13 @@ data class PdlSøkerData(val person: PdlSøker?)
 
 data class PersonDataBolk<T>(val ident: String, val code: String, val person: T?)
 data class PersonBolk<T>(val personBolk: List<PersonDataBolk<T>>)
-data class PdlBolkResponse<T>(val data: PersonBolk<T>?, val errors: List<PdlError>?) {
+data class PdlBolkResponse<T>(val data: PersonBolk<T>?, val errors: List<PdlError>?, val extensions: PdlExtensions?) {
 
     fun errorMessages(): String {
         return errors?.joinToString { it -> it.message } ?: ""
+    }
+    fun harAdvarsel(): Boolean {
+        return !extensions?.warnings.isNullOrEmpty()
     }
 }
 
