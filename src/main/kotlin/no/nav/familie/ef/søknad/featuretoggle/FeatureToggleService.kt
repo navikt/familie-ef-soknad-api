e285ed81e39788ee7d38e081680d7ec4f5d3ffea
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2023-09-14 13:45:14 +0200

    Unleash next (#894)
    Unleash next

--- src/main/kotlin/no/nav/familie/ef/søknad/featuretoggle/FeatureToggleService.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/featuretoggle/FeatureToggleService.kt
@@ -1,10 +1,16 @@
 package no.nav.familie.ef.søknad.featuretoggle
 
-interface FeatureToggleService {
+import no.nav.familie.unleash.DefaultUnleashService
+import org.springframework.stereotype.Service
+
+@Service
+class FeatureToggleService(val defaultUnleashService: DefaultUnleashService) {
 
     fun isEnabled(toggleId: String): Boolean {
-        return isEnabled(toggleId, false)
+        return defaultUnleashService.isEnabled(toggleId)
     }
 
-    fun isEnabled(toggleId: String, defaultValue: Boolean): Boolean
+    fun isEnabled(toggleId: String, defaultValue: Boolean): Boolean {
+        return defaultUnleashService.isEnabled(toggleId, defaultValue)
+    }
 }
