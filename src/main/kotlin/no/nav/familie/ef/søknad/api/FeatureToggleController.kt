270dc1aee20de5c55d9ea46cb1c84aea6ad9c720
ma10s <mattis.janitz@nav.no>
2023-10-24 09:30:57 +0200

    Vil legge til toggle - 'familie.ef.soknad.validerbosituasjon'

--- src/main/kotlin/no/nav/familie/ef/søknad/api/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/api/FeatureToggleController.kt
@@ -17,6 +17,7 @@ class FeatureToggleController(private val featureToggleService: FeatureToggleSer
     val funksjonsbrytere = listOf(
         "familie.ef.soknad.feilsituasjon",
         "familie.ef.soknad.nynorsk",
+        "familie.ef.soknad.validerbosituasjon",
     )
 
     @GetMapping
