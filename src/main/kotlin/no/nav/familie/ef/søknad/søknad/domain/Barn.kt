8a96a5e4f0099c13385a1ce94aca40d152776b2d
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2024-02-02 10:14:54 +0100

    Fjernet utdatert kommentar

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/domain/Barn.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/domain/Barn.kt
@@ -10,7 +10,7 @@ data class Barn(
     val lagtTil: Boolean? = false,
     val navn: TekstFelt?, // optional på nye barn
     val født: BooleanFelt,
-    val forelder: AnnenForelder? = null, // Dersom annen forelder er død så skal denne være null
+    val forelder: AnnenForelder? = null,
     val skalHaBarnepass: BooleanFelt?,
     val særligeTilsynsbehov: TekstFelt?,
     val barnepass: Barnepass?,
