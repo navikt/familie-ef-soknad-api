8265f241beb8c319be67160ce94cc99a7e585a4f
ma10s <mattis.janitz@nav.no>
2024-03-01 13:22:41 +0100

    Vil bruke riktig formatering Fjernet logger som ikke er i bruk

--- src/main/kotlin/no/nav/familie/ef/søknad/minside/SaksbehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/minside/SaksbehandlingController.kt
@@ -2,12 +2,9 @@ package no.nav.familie.ef.søknad.minside
 
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/saksbehandling")
