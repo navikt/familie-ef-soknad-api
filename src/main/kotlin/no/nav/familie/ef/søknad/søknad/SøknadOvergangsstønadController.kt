81b093adbae3c548b88fa8afee67274b47f118d8
ma10s <mattis.janitz@nav.no>
2024-10-10 10:34:51 +0200

    Trenger ikke logge søknad json

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/SøknadOvergangsstønadController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/SøknadOvergangsstønadController.kt
@@ -1,12 +1,10 @@
 package no.nav.familie.ef.søknad.søknad
 
 import no.nav.familie.ef.søknad.infrastruktur.exception.ApiFeil
-import no.nav.familie.ef.søknad.infrastruktur.featuretoggle.FeatureToggleService
 import no.nav.familie.ef.søknad.søknad.domain.Kvittering
 import no.nav.familie.ef.søknad.søknad.dto.SøknadOvergangsstønadDto
 import no.nav.familie.sikkerhet.EksternBrukerUtils
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.validation.annotation.Validated
@@ -22,9 +20,7 @@ import java.time.LocalDateTime
 @Validated
 class SøknadOvergangsstønadController(
     val søknadService: SøknadService,
-    val featureToggleService: FeatureToggleService,
 ) {
-    private val secureLogger = LoggerFactory.getLogger("secureLogger")
 
     @PostMapping
     fun sendInn(
@@ -32,10 +28,6 @@ class SøknadOvergangsstønadController(
     ): Kvittering {
         if (!EksternBrukerUtils.personIdentErLikInnloggetBruker(søknad.person.søker.fnr)) {
             throw ApiFeil("Fnr fra token matcher ikke fnr på søknaden", HttpStatus.FORBIDDEN)
-        }
-
-        if (featureToggleService.isEnabled("familie.ef.soknad-api-logg-stonad")) {
-            secureLogger.info("Mottatt søknad: $søknad ")
         }
 
         val innsendingMottatt = LocalDateTime.now()
