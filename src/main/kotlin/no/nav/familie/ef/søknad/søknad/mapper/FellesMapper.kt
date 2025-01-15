5aa00d65d7f6019afef9b1427317999f9a68580c
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-01-15 11:22:13 +0100

    Bump kontrakter og flyttet språk fra søknadMedVedlegg til innsendingsdetaljer

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/FellesMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/FellesMapper.kt
@@ -12,9 +12,10 @@ object FellesMapper {
     fun mapInnsendingsdetaljer(
         innsendingMottatt: LocalDateTime,
         datoPåbegyntSøknad: LocalDate? = null,
+        språk: String? = null,
     ): Søknadsfelt<Innsendingsdetaljer> =
         Søknadsfelt(
             InnsendingsdetaljerTekst.hentTekst(),
-            Innsendingsdetaljer(Søknadsfelt(DatoMottatt.hentTekst(), innsendingMottatt), datoPåbegyntSøknad),
+            Innsendingsdetaljer(Søknadsfelt(DatoMottatt.hentTekst(), innsendingMottatt), datoPåbegyntSøknad, språk),
         )
 }
