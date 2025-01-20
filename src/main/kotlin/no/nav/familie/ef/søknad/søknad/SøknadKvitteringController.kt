9363eae166e22448ebb4b5d3148bfa76780ffe8a
Viktor Solberg <viktorgsolberg@gmail.com>
2025-01-20 15:46:51 +0100

    fjerner annotering som utilgjengeliggjør søknadskvitteringcontroller i prod

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/SøknadKvitteringController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/SøknadKvitteringController.kt
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 // TODO: Endre endepunkt til /api/soknad etter at de andre søknadscontrollerne er utfaset
-@Profile("!prod")
 @RestController
 @RequestMapping(path = ["/api/soknadskvittering"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @ProtectedWithClaims(issuer = EksternBrukerUtils.ISSUER_TOKENX, claimMap = ["acr=Level4"])
