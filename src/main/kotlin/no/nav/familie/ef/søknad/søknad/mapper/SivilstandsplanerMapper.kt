3a4fc2708989b8c03f13997e084977b255e2ecd7
Sandra Lekve <sandra.lekve@icloud.com>
2025-01-28 13:52:04 +0100

    skjul-overskrift-ved-tomme-verdier

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/SivilstandsplanerMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/SivilstandsplanerMapper.kt
@@ -6,11 +6,20 @@ import no.nav.familie.ef.søknad.utils.Språktekster.Fremtidsplaner
 import no.nav.familie.ef.søknad.utils.tilSøknadsfelt
 import no.nav.familie.kontrakter.ef.søknad.Sivilstandsplaner
 
-object SivilstandsplanerMapper : Mapper<Bosituasjon, Sivilstandsplaner>(Fremtidsplaner) {
-    override fun mapDto(data: Bosituasjon): Sivilstandsplaner =
-        Sivilstandsplaner(
-            harPlaner = data.skalGifteSegEllerBliSamboer?.tilSøknadsfelt(),
-            fraDato = data.datoSkalGifteSegEllerBliSamboer?.tilSøknadsfelt(),
-            vordendeSamboerEktefelle = data.vordendeSamboerEktefelle?.let(PersonMinimumMapper::map),
-        )
+object SivilstandsplanerMapper : Mapper<Bosituasjon, Sivilstandsplaner?>(Fremtidsplaner) {
+    override fun mapDto(data: Bosituasjon): Sivilstandsplaner? {
+        val harPlaner = data.skalGifteSegEllerBliSamboer?.tilSøknadsfelt()
+        val fraDato = data.datoSkalGifteSegEllerBliSamboer?.tilSøknadsfelt()
+        val vordendeSamboerEktefelle = data.vordendeSamboerEktefelle?.let(PersonMinimumMapper::map)
+
+        return if (harPlaner == null && fraDato == null && vordendeSamboerEktefelle == null) {
+            null
+        } else {
+            Sivilstandsplaner(
+                harPlaner = harPlaner,
+                fraDato = fraDato,
+                vordendeSamboerEktefelle = vordendeSamboerEktefelle
+            )
+        }
+    }
 }
