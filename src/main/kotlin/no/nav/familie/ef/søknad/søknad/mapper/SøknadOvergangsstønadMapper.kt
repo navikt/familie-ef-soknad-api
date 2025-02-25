2c0fd31e710a25ba7a3ded053b9e56ef48d6ad12
krikof-nav <kristian.kofoed@nav.no>
2025-02-25 11:33:55 +0100

    Logging for debugging av språk for overgangstønad.

--- src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/SøknadOvergangsstønadMapper.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/søknad/mapper/SøknadOvergangsstønadMapper.kt
@@ -1,15 +1,19 @@
 package no.nav.familie.ef.søknad.søknad.mapper
 
+import no.nav.familie.ef.søknad.infrastruktur.config.ApplicationConfig
 import no.nav.familie.ef.søknad.søknad.dto.SøknadOvergangsstønadDto
 import no.nav.familie.ef.søknad.søknad.mapper.StønadsstartMapper.mapStønadsstart
 import no.nav.familie.ef.søknad.utils.DokumentasjonWrapper
 import no.nav.familie.ef.søknad.utils.Språk
 import no.nav.familie.ef.søknad.utils.kontekst
 import no.nav.familie.ef.søknad.utils.lagDokumentasjonWrapper
+import no.nav.familie.ef.søknad.utils.språk
 import no.nav.familie.ef.søknad.utils.tilKontrakt
 import no.nav.familie.kontrakter.ef.søknad.SøknadMedVedlegg
 import no.nav.familie.kontrakter.ef.søknad.SøknadOvergangsstønad
 import no.nav.familie.kontrakter.ef.søknad.validering.OvergangsstønadValidering
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
 
@@ -22,6 +26,9 @@ class SøknadOvergangsstønadMapper {
     ): SøknadMedVedlegg<SøknadOvergangsstønad> {
         kontekst.set(Språk.fromString(dto.locale))
         val vedlegg: Map<String, DokumentasjonWrapper> = lagDokumentasjonWrapper(dto.dokumentasjonsbehov)
+
+        logger.info("DEBUG MODE --- SøknadOvergamgstønadDto sin locale er: ${dto.locale}")
+        logger.info("DEBUG MODE --- Språkkontekst er: ${språk()}")
 
         val søknad =
             SøknadOvergangsstønad(
@@ -53,5 +60,9 @@ class SøknadOvergangsstønadMapper {
             dto.dokumentasjonsbehov.tilKontrakt(),
             dto.skalBehandlesINySaksbehandling,
         )
+    }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(ApplicationConfig::class.java)
     }
 }
