cfe9600b9987c4e2bd2a7ce35de384290ceebddd
endrehm <endre.heien.mo@nav.no>
2025-03-24 15:51:55 +0100

    What?

--- src/main/kotlin/no/nav/familie/ef/søknad/infrastruktur/featuretoggle/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/infrastruktur/featuretoggle/FeatureToggleController.kt
@@ -1,7 +1,7 @@
 package no.nav.familie.ef.søknad.infrastruktur.featuretoggle
 
 import no.nav.familie.unleash.UnleashService
-import no.nav.security.token.support.core.api.ProtectedWithClaims
+import no.nav.security.token.support.core.api.Unprotected
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping(path = ["/api/featuretoggle"], produces = [MediaType.APPLICATION_JSON_VALUE])
-@ProtectedWithClaims(issuer = "azuread")
+@Unprotected
 class FeatureToggleController(
     private val unleashService: UnleashService,
 ) {
