7c5cdc00d4ae9897c344cb28e99331d8cbf84a38
RobinLundSadun <robin.lund.sadun@gmail.com>
2026-06-24 14:49:42 +0200

    Legger til gammelt endepunkt så søknaden ikke tryner under merge/deploy

--- src/main/kotlin/no/nav/familie/ef/søknad/minside/SaksbehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/minside/SaksbehandlingController.kt
@@ -18,6 +18,11 @@ class SaksbehandlingController(
     @GetMapping("/har-overgangsstonad-pa-gammelt-regelverk")
     fun harOvergangsstønadPåGammeltRegelverk() = saksbehandlingService.harOvergangsstønadPåGammeltRegelverk()
 
+    // TODO skal slettes
+    @Deprecated("Erstattet av har-overgangsstonad-pa-gammelt-regelverk. Returnerer alltid JA for å sikre gammel flyt for allerede deployede klienter.")
+    @GetMapping("/har-vedtak-pa-gammelt-regelverk")
+    fun harVedtakPåGammeltRegelverkLegacy() = "JA"
+
     @GetMapping("/har-gyldig-barnetilsyn-ved-regelendring")
     fun harGyldigBarnetilsynVedRegelendring() = saksbehandlingService.harGyldigBarnetilsynVedRegelendring()
 }
