042fa1880de67e4a1fc5f40663a843cf24526f93
Robin Lund Sadun <47403589+RobinLundSadun@users.noreply.github.com>
2026-06-25 12:27:21 +0200

    Endepunktet for å sjekke om bruker har løpende barnetilsyn 30.06.2026 (#1264)
    * Endepunktet for å sjekke om bruker har løpende barnetilsyn 30.06.2026
    
    * Oppdaterer url til bedre å matche vi vi gjør
    
    * Renamer funksjonalitet for å bedre matche at vi henter overgangsstonad og ikke alle vedtak
    
    * Legger til gammelt endepunkt så søknaden ikke tryner under merge/deploy
    
    * Fjerner ø

--- src/main/kotlin/no/nav/familie/ef/søknad/infrastruktur/config/SaksbehandlingConfig.kt
+++ src/main/kotlin/no/nav/familie/ef/søknad/infrastruktur/config/SaksbehandlingConfig.kt
@@ -9,7 +9,8 @@ data class SaksbehandlingConfig(
     val uri: URI,
 ) {
     internal val hentStønadsperioderUri = byggUri(PATH_HENT_STØNADSPERIODER)
-    internal val harVedtakPåGammeltRegelverkUri = byggUri(PATH_HAR_VEDTAK_PÅ_GAMMELT_REGELVERK)
+    internal val harOvergangsstønadPåGammeltRegelverkUri = byggUri(PATH_HAR_OVERGANGSSTØNAD_PÅ_GAMMELT_REGELVERK)
+    internal val harGyldigBarnetilsynVedRegelendringUri = byggUri(PATH_HAR_GYLDIG_BARNETILSYN_VED_REGELENDRING)
     internal val pingUri = byggUri(PATH_PING)
 
     private fun byggUri(path: String) =
@@ -21,7 +22,8 @@ data class SaksbehandlingConfig(
 
     companion object {
         private const val PATH_HENT_STØNADSPERIODER = "/ekstern/minside/stonadsperioder"
-        private const val PATH_HAR_VEDTAK_PÅ_GAMMELT_REGELVERK = "/ekstern/soknad/har-vedtak-pa-gammelt-regelverk"
+        private const val PATH_HAR_OVERGANGSSTØNAD_PÅ_GAMMELT_REGELVERK = "/ekstern/soknad/har-overgangsstonad-pa-gammelt-regelverk"
+        private const val PATH_HAR_GYLDIG_BARNETILSYN_VED_REGELENDRING = "/ekstern/soknad/har-gyldig-barnetilsyn-ved-regelendring"
         private const val PATH_PING = "ping"
     }
 }
