1a82b0c2cf48b603b2e1921924b0ae10fc088916
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-07-30 08:38:23 +0200

    Byttet token-issuer fra MockOAuth2Server til fakedings, bruker fakedings for å generere tokens lokalt.

--- src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
+++ src/test/kotlin/no/nav/familie/ef/søknad/ApplicationLocalLauncher.kt
@@ -1,17 +1,10 @@
 package no.nav.familie.ef.søknad
 
-import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.springframework.boot.builder.SpringApplicationBuilder
 
 class ApplicationLocalLauncher : ApplicationLocalTestLauncher()
 
-private val mockOauth2ServerPort: Int = 11588
-
 fun main(args: Array<String>) {
-    val mockOAuth2Server = MockOAuth2Server()
-    mockOAuth2Server.start(mockOauth2ServerPort)
-    Runtime.getRuntime().addShutdownHook(Thread { mockOAuth2Server.shutdown() })
-
     SpringApplicationBuilder(ApplicationLocalLauncher::class.java)
         .profiles(
             "local",
